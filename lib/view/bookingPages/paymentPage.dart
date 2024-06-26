c274ee8576b8f6bb29184697f46c3ae4fd7ea158
ALLU-YT <ameenofficial33@gmail.com>
2024-06-26 18:16:31 +0530

    commit154

---
+++ lib/view/bookingPages/paymentPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookingPaymentPage extends StatelessWidget {
+  const BookingPaymentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+    );
+  }
+}
